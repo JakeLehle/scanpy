2529c2122ee50e34841165c11ad40c9fcb566904
Jake Lehle, Ph.D. <84940857+JakeLehle@users.noreply.github.com>
2025-03-16 19:58:46 -0500

    Add bws_test method (scverse#3503)

--- src/scanpy/tools/_rank_genes_groups.py
+++ src/scanpy/tools/_rank_genes_groups.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING, Literal
 import numpy as np
 import pandas as pd
 from scipy.sparse import issparse, vstack
+from scipy.stats import bws_test 
 
 from .. import _utils
 from .. import logging as logg
